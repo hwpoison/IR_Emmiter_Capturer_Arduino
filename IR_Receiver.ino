60dd27f71bacca31dad9137acd064c5d3646e2b0
hwpoison <guichezero@gmail.com>
2025-04-28 00:47:13 -0300

    IR emmit/reception added

--- IR_Receiver.ino
+++ IR_Receiver.ino
@@ -1,22 +1,18 @@
-/* 
-This code intends to demostrate how the IR sensor works with a VS1838B sensor.
-You can use it to clone the IR Remote codes.
+/* VS1838B sensor
+.......(H).......
+.....|..|..|.....
+... |...|...|....
+...|....|....|...
+...|....|....|...
+..IN..GND..VCC...
 
-::VS1838B scheme::
-.......(H)........
-.....|..|..|......
-... |...|...|.....
-...|....|....|....
-...|....|....|....
-..OUT..GND..VCC...
-
-OUT = PWM Pin 11
-VCC = 2.7V-5.5V
+IN = PWM Pin 11
+VCC= 2.7V-5.5V
 
 Datasheet = https://www.alldatasheet.es/datasheet-pdf/pdf/1132465/ETC2/VS1838B.html
-
+NEC Specifications = https://www.sbprojects.net/knowledge/ir/nec.php
 */
-const int RECV_PIN = 11;  // IR Sensor OUT
+const int RECV_PIN = 11;  // IR Sensor IN
 
 void setup() {
   Serial.begin(9600);
@@ -31,8 +27,9 @@ void loop() {
 }
 
 bool readIR(unsigned long *result) {
-  // NEC = Start pulse: 9 ms HIGH + 4.5 ms LOW.
+  /// 1 - Receive the start signal
 
+  // NEC = Start pulse: 9 ms HIGH + 4.5 ms LOW.
   // PIN HIGH = Withouth signal
   // PIN LOW  = Active signal
 
@@ -53,10 +50,12 @@ bool readIR(unsigned long *result) {
   startTime = micros();
   while (digitalRead(RECV_PIN) == HIGH);
 
+  // 2 - Recive the second signal
   // Now that the RECV_PIN received the second signal (that contains the data)
   // We will calculate the time diff so we get the second timestamp
   if (micros() - startTime < 4000) return false;
 
+  // 3 - Start receving data
   // Now NEC start is confirmed
   // Start 32bit data read
   for (int i = 0; i < 32; i++) {
@@ -70,6 +69,6 @@ bool readIR(unsigned long *result) {
       *result |= (1UL << (31 - i));
     }
   }
-
+  
   return true;
 }
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-## VS1838B IR sensor
-This code intends to demostrate how the IR sensor works with a VS1838B sensor.
-You can use it to clone the IR Remote codes.
+## IR Transmission / Emission
+This codes intends to demostrate how the IR NEC protocol works using Arduino.
+
+1 - Emisor with a simple IR LED
+2 - Reception using the VS1838B sensor
 
 VS1838B Datasheet = https://www.alldatasheet.es/datasheet-pdf/pdf/1132465/ETC2/VS1838B.html
+NEC Specifications = https://www.sbprojects.net/knowledge/ir/nec.php
